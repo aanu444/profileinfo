f70dc543fe465cdb99b0cc7d689b9116af4662d8
Omolola Odunowo <omololaodunowo@gmail.com>
2022-06-21 11:49:37 +0100

    uploading profile information js prompt

---
+++ script.js
@@ -0,0 +1,15 @@
+function profileInformation(){
+  const fullName=prompt("What is your fullname?");
+  alert("Your full name is " + fullName);
+
+  const height=prompt("How tall are you?");
+  alert ("Your current height is " + height);
+
+  const country=prompt("What country are you from?");
+  alert("You come from " + country);
+
+  alert("Thank you for inputting the necessary information");
+
+}
+
+profileInformation();
